cafb133fdd1ce4e5b1e4a259747a6dcad261fb37
HEIGE-PCloud <52968553+HEIGE-PCloud@users.noreply.github.com>
2022-12-05 21:25:15 +0000

    refactor: file

--- src/main/kotlin/pawnrace/File.kt
+++ src/main/kotlin/pawnrace/File.kt
@@ -1,17 +1,8 @@
 package pawnrace
 
-class File(col: Int) {
+data class File(val value: Int) {
   constructor(pos: Char) : this(pos.lowercaseChar().code - 'a'.code)
-
-  val value = col
 
   override fun toString(): String =
     (value + 'a'.code).toChar().toString()
-
-  override fun equals(other: Any?): Boolean = when (other) {
-    is File -> other.value == value
-    else -> false
-  }
-
-  override fun hashCode(): Int = value.hashCode()
 }
