6c80a8159f94c97c51f38b3cc54acf6b568b4850
HEIGE-PCloud <52968553+HEIGE-PCloud@users.noreply.github.com>
2022-12-05 21:31:17 +0000

    refactor: make rank data class

--- src/main/kotlin/pawnrace/Rank.kt
+++ src/main/kotlin/pawnrace/Rank.kt
@@ -1,16 +1,8 @@
 package pawnrace
 
-class Rank(row: Int) {
+data class Rank(val value: Int) {
   constructor(pos: Char) : this(pos.digitToInt() - 1)
-  val value = row
 
   override fun toString(): String =
     (value + 1).toString()
-
-  override fun equals(other: Any?): Boolean = when(other) {
-    is Rank -> other.value == value
-    else -> false
-  }
-
-  override fun hashCode(): Int = value.hashCode()
 }
