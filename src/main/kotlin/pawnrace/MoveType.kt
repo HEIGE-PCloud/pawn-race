ca65fc287dc4f1a43dbe3eb27d62bf324aae941c
HEIGE-PCloud <52968553+HEIGE-PCloud@users.noreply.github.com>
2022-12-05 21:37:07 +0000

    refactor: add more comments

--- src/main/kotlin/pawnrace/MoveType.kt
+++ src/main/kotlin/pawnrace/MoveType.kt
@@ -1,5 +1,10 @@
 package pawnrace
 
+/**
+ * PEACEFUL: when no capture is involved
+ * CAPTURE: when a regular capture happens
+ * EN_PASSANT: when there is a capture following the En Passant rule
+ */
 enum class MoveType {
   PEACEFUL, CAPTURE, EN_PASSANT
 }
--- src/main/kotlin/pawnrace/Position.kt
+++ src/main/kotlin/pawnrace/Position.kt
@@ -4,8 +4,16 @@ class Position(row: Int, col: Int) {
   val rank = Rank(row)
   val file = File(col)
 
+  /**
+   * The constructor takes this parameter and initialises a position
+   * with the given coordinates in the short algebraic notation,
+   * for example A4 or a4.
+   */
   constructor(pos: String) : this(Rank(pos[1]).value, File(pos[0]).value)
 
+  /**
+   * Returns the short algebraic notation of this position.
+   */
   override fun toString(): String =
     file.toString() + rank.toString()
 
