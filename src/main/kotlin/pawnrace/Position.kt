6c18e5ca11414c2a92f78ddce086219f20e20097
HEIGE-PCloud <52968553+HEIGE-PCloud@users.noreply.github.com>
2022-12-05 21:35:39 +0000

    refactor: make Position better

--- src/main/kotlin/pawnrace/Position.kt
+++ src/main/kotlin/pawnrace/Position.kt
@@ -1,10 +1,11 @@
 package pawnrace
 
 class Position(row: Int, col: Int) {
-  val file = File(col)
   val rank = Rank(row)
+  val file = File(col)
 
   constructor(pos: String) : this(Rank(pos[1]).value, File(pos[0]).value)
+
   override fun toString(): String =
     file.toString() + rank.toString()
 
--- src/test/kotlin/pawnrace/PositionTest.kt
+++ src/test/kotlin/pawnrace/PositionTest.kt
@@ -5,8 +5,16 @@ import kotlin.test.assertEquals
 
 class PositionTest {
   @Test
+  fun `test init`() {
+    assertEquals(File('a'), Position("A1").file)
+    assertEquals(Rank('1'), Position("A1").rank)
+  }
+
+  @Test
   fun `test toString`() {
     assertEquals("a1", Position("A1").toString())
     assertEquals("a1", Position("a1").toString())
+    assertEquals("a2", Position("A2").toString())
+    assertEquals("a2", Position("a2").toString())
   }
 }
