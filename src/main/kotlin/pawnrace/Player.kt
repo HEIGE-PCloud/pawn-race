272c233832f69f09ea15bbd3e3c048aa231d9e3c
HEIGE-PCloud <52968553+HEIGE-PCloud@users.noreply.github.com>
2022-12-06 15:37:34 +0000

    chore: use Int for evaluate

--- src/main/kotlin/pawnrace/Player.kt
+++ src/main/kotlin/pawnrace/Player.kt
@@ -3,94 +3,94 @@ package pawnrace
 import java.util.concurrent.ConcurrentHashMap
 import kotlin.math.abs
 import kotlin.math.max
-import kotlin.math.pow
 
 class Player(val piece: Piece, var opponent: Player? = null) {
-  private val evaluateCache: ConcurrentHashMap<Game, Double> = ConcurrentHashMap()
+  private val evaluateCache: ConcurrentHashMap<Game, Int> = ConcurrentHashMap()
   private fun getAllPawns(board: Board): List<Position> = board.positionsOf(piece)
   private fun getAllValidMoves(game: Game): List<Move> = game.moves(piece)
 
-  fun isPassedPawn(board: Board, pos: Position): Boolean {
-    if (board.pieceAt(pos) != piece) return false
+  fun isPassedPawn(board: Board, pos: Position): Int {
     val startRow = pos.rank.value + piece.direction()
     val endRow = piece.promotionRank()
     val col = pos.file.value
     val leftCol = col - 1
     val rightCol = col + 1
-    for (i in startRow..endRow step piece.direction()) {
-      if (board.pieceAt(Position(i, col)) != null) return false
-      if (leftCol >= 0 && board.pieceAt(Position(i, leftCol)) == piece.opposite()) return false
-      if (rightCol >= 0 && board.pieceAt(Position(i, rightCol)) == piece.opposite()) return false
-    }
-    return true
+//    for (i in startRow..endRow) {
+//      if (board.pieceAt(Position(i, col)) != null) return 0
+//      if (leftCol >= 0 && board.pieceAt(Position(i, leftCol)) == piece.opposite()) return 0
+//      if (rightCol >= 0 && board.pieceAt(Position(i, rightCol)) == piece.opposite()) return 0
+//    }
+    return 0
   }
 
-  private fun evaluate(game: Game): Double {
+  private fun evaluate(game: Game): Int {
     val cache = evaluateCache[game]
     if (cache != null) return cache
 
     val positions = getAllPawns(game.board)
-    if (positions.find { it.rank.value == piece.promotionRank() } != null) return 1e9
+    if (positions.find { it.rank.value == piece.promotionRank() } != null) return 1000000000
 
-    val value = positions.fold(0.0) { acc, pos ->
-      acc + (8.0).pow(abs(pos.rank.value - piece.startingRank()))
+
+    val moves = getAllValidMoves(game)
+    val value = positions.fold(0) { acc, pos ->
+      val dist = abs(pos.rank.value - piece.startingRank())
+      acc + dist * dist + dist * dist * isPassedPawn(game.board, pos)
     }
     evaluateCache[game] = value
     return value
   }
 
-  fun quiesce(game: Game, a: Int, beta: Int): Int {
+  private fun quiesce(game: Game, a: Int, beta: Int): Int {
     var alpha = a
     val stand_pat = evaluate(game)
-    if(stand_pat >= beta)
+    if (stand_pat >= beta)
       return beta
-    if(alpha < stand_pat)
-      alpha = stand_pat.toInt()
+    if (alpha < stand_pat)
+      alpha = stand_pat
     val captures = getAllValidMoves(game).filter { it.type == MoveType.CAPTURE || it.type == MoveType.EN_PASSANT }
-    for(capture in captures)  {
+    for (capture in captures) {
       val nextGame = game.applyMove(capture)
-      val score = -quiesce(nextGame, -beta, -alpha )
-      if( score >= beta )
+      val score = -quiesce(nextGame, -beta, -alpha)
+      if (score >= beta)
         return beta
-      if( score > alpha )
+      if (score > alpha)
         alpha = score
     }
     return alpha
   }
 
-  fun alphaBeta(game: Game, a: Int, beta: Int, depthleft: Int ): Int {
+  private fun alphaBeta(game: Game, a: Int, beta: Int, depth: Int): Int {
     var alpha = a
-    if (depthleft == 0) return quiesce(game, alpha, beta)
+    if (depth == 0) return quiesce(game, alpha, beta)
     val moves = getAllValidMoves(game)
-    for (move in moves)  {
-      val score = -alphaBeta(game, -beta, -alpha, depthleft - 1 )
+    for (move in moves) {
+      val score = -alphaBeta(game, -beta, -alpha, depth - 1)
       if (score >= beta)
-        return beta   //  fail hard beta-cutoff
-      if (score > alpha)
-        alpha = score // alpha acts like max in MiniMax
+        return beta
+      alpha = max(alpha, score)
     }
     return alpha
   }
 
-  private fun negaMax(game: Game, depth: Int): Double {
+  private fun negaMax(game: Game, depth: Int): Int {
     if (depth == 0) return evaluate(game)
-    var maxScore = Double.MIN_VALUE
+    var maxScore = Int.MIN_VALUE
     val allMoves = getAllValidMoves(game)
     for (move in allMoves) {
-      maxScore = max(-1 * alphaBeta(game, Int.MIN_VALUE, Int.MAX_VALUE, depth - 1).toDouble(), maxScore)
+      maxScore = max(-1 * alphaBeta(game, Int.MIN_VALUE, Int.MAX_VALUE, depth - 1), maxScore)
     }
     return maxScore
   }
 
+  private fun randomMove(game: Game): Move = getAllValidMoves(game).random()
   fun makeMove(game: Game): Move {
     val moves = getAllValidMoves(game)
     val games = moves.map { game.applyMove(it) }
-    val scores = games.map { negaMax(it, 7) }
+    val scores = games.map { negaMax(it, 8) }
     val maxScore = scores.max()
     val maxIndex = scores.indexOf(maxScore)
     return moves[maxIndex]
   }
-
   override fun toString(): String {
     return "Player $piece"
   }
