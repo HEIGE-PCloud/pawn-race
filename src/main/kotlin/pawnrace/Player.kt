e156bbcee735dc9ed467892dc27bfa6e014aeaed
HEIGE-PCloud <52968553+HEIGE-PCloud@users.noreply.github.com>
2022-12-07 12:51:12 +0000

    feat: implement time limit and concurrency

--- src/main/kotlin/pawnrace/Player.kt
+++ src/main/kotlin/pawnrace/Player.kt
@@ -1,15 +1,28 @@
 package pawnrace
 
-import java.util.concurrent.ConcurrentHashMap
-import java.util.concurrent.ExecutorService
+import java.util.concurrent.*
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.atomic.AtomicReference
 import kotlin.math.abs
 import kotlin.math.max
 
 
+// Use a much smaller INT_MAX to avoid overflow
+const val INT_MAX = 100000000
+const val INT_MIN = -100000000
+
 class Player(val piece: Piece, var opponent: Player? = null) {
+  private var run = true
   private val evaluateCache: ConcurrentHashMap<Game, Int> = ConcurrentHashMap()
   private fun getAllPawns(board: Board): List<Position> = board.positionsOf(piece)
-  private fun getAllValidMoves(game: Game): List<Move> = game.moves(piece).sortedByDescending { it.type.ordinal }
+
+  /**
+   * get all valid moves and sort them in such order that
+   * better moves should come first for better alpha beta pruning
+   * a capture move with high rank value should be a better move
+   */
+  private fun getAllValidMoves(game: Game): List<Move> =
+    game.moves(piece).sortedByDescending { it.type.ordinal + it.to.rank.value }
 
 
   /**
@@ -37,14 +50,12 @@ class Player(val piece: Piece, var opponent: Player? = null) {
   private fun evaluate(game: Game): Int {
     val cache = evaluateCache[game]
     if (cache != null) return cache
-
-    val positions = getAllPawns(game.board)
-    if (positions.find { it.rank.value == piece.promotionRank() } != null) return 1000000000
-
     val moves = getAllValidMoves(game)
     val value = moves.fold(0) { acc, move ->
       var score = move.distance() * 2 // 1 or 2
-      if (move.to.rank.value == piece.promotionRank()) score += 10000
+      if (move.to.rank.value == piece.promotionRank() ||
+        move.from.rank.value == piece.promotionRank()
+      ) score += 1000
       // if capture +5
       if (move.type != MoveType.PEACEFUL) {
         score += 10
@@ -72,6 +83,9 @@ class Player(val piece: Piece, var opponent: Player? = null) {
     return value
   }
 
+  /**
+   * Use Quiescence Search to only evaluate quiet moves to avoid the horizon effect
+   */
   private fun quiesce(game: Game, a: Int, beta: Int): Int {
     var alpha = a
     val standPat = evaluate(game)
@@ -81,6 +95,7 @@ class Player(val piece: Piece, var opponent: Player? = null) {
       alpha = standPat
     val captures = getAllValidMoves(game).filter { it.type == MoveType.CAPTURE || it.type == MoveType.EN_PASSANT }
     for (capture in captures) {
+      if (!run) break
       val nextGame = game.applyMove(capture)
       val score = -quiesce(nextGame, -beta, -alpha)
       if (score >= beta)
@@ -96,6 +111,7 @@ class Player(val piece: Piece, var opponent: Player? = null) {
     if (depth == 0) return quiesce(game, alpha, beta)
     val moves = getAllValidMoves(game)
     for (move in moves) {
+      if (!run) break
       val score = -alphaBeta(game, -beta, -alpha, depth - 1)
       if (score >= beta)
         return beta
@@ -104,36 +120,33 @@ class Player(val piece: Piece, var opponent: Player? = null) {
     return alpha
   }
 
-  private fun negaMax(game: Game, depth: Int): Int {
-    if (depth == 0) evaluate(game)
-    var maxScore = Int.MIN_VALUE
-    val allMoves = getAllValidMoves(game)
-    for (move in allMoves) {
-      maxScore = max(-1 * alphaBeta(game, Int.MIN_VALUE, Int.MAX_VALUE, depth - 1), maxScore)
-    }
-    return maxScore
-  }
+  private fun randomMove(game: Game): Move = getAllValidMoves(game).random()
 
-
-  //  private fun randomMove(game: Game): Move = getAllValidMoves(game).random()
-  fun makeMove(game: Game, executorService: ExecutorService): Move {
-
-    val MAX_DEPTH = 8
+  fun makeMove(game: Game, executor: ExecutorService): Move {
+    val maxDepth = 30
     val moves = getAllValidMoves(game)
     val games = moves.map { game.applyMove(it) }
-    var maxScore = 0
-    var maxIndex = 0
-    for (depth in 1..MAX_DEPTH) {
-      val scores = games.map { negaMax(it, depth) }
-//      val futureScores = games.map { executorService.submit(negaMax(it, depth)) }
-//      val scores = futureScores.map { it.get() }
-      val maxDepthScore = scores.max()
-      if (maxDepthScore > maxScore) {
-        maxScore = maxDepthScore
-        maxIndex = scores.indexOf(maxScore)
+    val bestScore = AtomicInteger(INT_MIN)
+    val bestMove: AtomicReference<Move?> = AtomicReference(null)
+    run = true
+    for (depth in 1..maxDepth) {
+      for (i in moves.indices) {
+        val currentMove = moves[i]
+        val currentGame = games[i]
+        executor.submit {
+          val score = alphaBeta(currentGame, INT_MIN, INT_MAX, depth)
+          if (score > bestScore.get()) {
+            bestScore.set(score)
+            bestMove.set(currentMove)
+          }
+        }
       }
     }
-    return moves[maxIndex]
+    if (!executor.awaitTermination(4500, TimeUnit.MILLISECONDS)) {
+      run = false
+    }
+
+    return bestMove.get() ?: randomMove(game)
   }
 
   override fun toString(): String {
